28002e1ad49229351ac9dcb73b4a042fd79e6684
MohamedAmr23 <amrm96754@gmail.com>
2025-08-11 08:01:36 +0300

    Simple Pig Latin

---
+++ Simple-Pig-Latin.js
@@ -0,0 +1,8 @@
+function pigIt(str){
+    return str.split(' ').map(word => {
+        if (word.match(/[a-zA-Z]/)) {
+        return word.slice(1) + word[0] + 'ay';
+        }
+        return word;
+    }).join(' ');
+}
